1b3f3049e077b795b99798fa301091fd02a6c88e
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-09 19:25:36 -0700

    task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+error_message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
