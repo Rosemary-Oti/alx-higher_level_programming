c2430539248176ee3d01866ca34094cecbeef682
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-16 17:45:24 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 import sys
 
 num_args = len(sys.argv) - 1
-args_list = sys.argv[1:]
 
-print(f"Number of argument(s): {num_args}")
 if num_args == 0:
     print("0 arguments.")
-else:
+elif num_args == 1:
     print("1 argument:")
+else:
+    print(f"{num_args} arguments:")
     for i in range(num_args):
         print(f"{i + 1}: {sys.argv[i + 1]}")
