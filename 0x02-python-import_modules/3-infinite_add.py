c9318b08f6e7342aef72d5d5366a0e02a06e4597
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-16 19:01:53 -0700

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    args = sys.argv[1:]
+    total_sum = sum(int(arg) for arg in args)
+    print(total_sum)
+
+if __name__ == "__main__":
+    main()
