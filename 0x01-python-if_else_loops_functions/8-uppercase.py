4cfb89efb9cc4f66e1019db4454ca217bf2ad64f
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-18 05:37:37 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(s):
+    offset = ord('A') - ord('a')
     for c in s:
         if 'a' <= c <= 'z':
-            offset = ord('A') - ord('a')
             print(chr(ord(c) + offset), end="")
         else:
             print(c, end="")
