2747e85ee884e8f938289af75e5fadb3890a6652
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-18 07:09:26 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# 9-print_last_digit.py
 
 def print_last_digit(number):
-	last_digit = abs(number) % 10
-	print(last_digit, end="")
-	return last_digit
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
