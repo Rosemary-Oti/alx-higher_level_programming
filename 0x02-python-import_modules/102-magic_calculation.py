d601118a8b6668a4081c5d2a893c5081345be39e
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-18 16:27:04 -0700

    task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,6 +2,6 @@
 
 def magic_calculation(a, b):
     if a < b:
-        add = __import__('magic_calculation_102').add
-        return add(a, b)
-    return __import__('magic_calculation_102').sub(a, b)
+    from magic_calculation_102 import add
+    return add(a, b)
+    return sub(a, b)
