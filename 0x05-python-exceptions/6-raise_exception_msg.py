c2c48a4779139f72fb1b59de33031089e9deba4e
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-28 16:24:53 -0700

    task 5

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-	'''Function that raise a NameError exception with a message'''
+	'''Raise a NameError exception with a message'''
 	raise NameError(message)
