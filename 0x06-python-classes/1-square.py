11f07219843f9dcbff0a628b8d9fa3be06d3e471
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-29 20:23:12 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represent a Square."""
+
+    def __init__(self, size):
+        """Represent the private instance attribute of the Square"""
+        self.__size = size    
