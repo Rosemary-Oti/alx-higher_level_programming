e4386ea08fc3b262a422a95c5d7d105aa6a8dca6
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-30 14:00:25 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,12 @@ class Square:
         Args:
             size (int, optional): The size of the square. Default is 0.
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         elif size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
