53f046be6a025851752e56940d7f5e83a4d32cc4
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-30 14:44:44 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 class Square:
     """Represents a square."""
@@ -32,12 +33,4 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size ** 2
 
