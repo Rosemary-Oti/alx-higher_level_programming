185eab6586edc4fc23aa01e5aeb4029fb3c8750a
Rosemary-Oti <otirosemary58@gmail.com>
2023-08-30 16:50:44 -0700

    task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def safe_function(fct, *args):
     try:
         result = fct(*args)
@@ -9,4 +10,3 @@ def safe_function(fct, *args):
     except Exception as e:
         print(f"Exception: {e}", file=sys.stderr)
         return None
-
